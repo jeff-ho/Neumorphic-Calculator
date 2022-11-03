0d8c1d0b9a8cd42998ec6606bf50c596eb7970aa
Jeffrey <jeff.ho@hotmail.com>
2022-11-03 18:41:02 -0400

    Create form template for number input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [formValue, setFormValue] = useState("")
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValue)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='counter'>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>Counter</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
+
+      <form onSubmit={handleSubmit}>
+        <label>Form Value</label>
+        <input 
+        type="text" 
+        value={formValue}
+        onChange={(e) => setFormValue(e.target.value)}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    
     </div>
   );
 }
